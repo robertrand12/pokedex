b97d3345decece7eded9b3c87862a4c11ad9cfb2
Bertrand Robert <brobert.bertrand@wanadoo.fr>
2023-03-28 12:09:07 +0200

    second props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -20,7 +18,7 @@ function App() {
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
