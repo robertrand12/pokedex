a00d9206171e7452684f28df46a5e46852b6f76b
Bertrand Robert <brobert.bertrand@wanadoo.fr>
2023-03-28 11:40:54 +0200

    first props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   );
 }
